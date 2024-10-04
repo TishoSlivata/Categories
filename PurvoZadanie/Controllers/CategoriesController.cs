a2fb1dd03207ad2b8d41e30626aecb00767d5da1
TishoSlivata <tihcho4@gmail.com>
2024-10-04 12:06:30 +0300

    First commit

---
+++ PurvoZadanie/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PurvoZadanie.Data;
+using PurvoZadanie.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class CategoriesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoriesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Categories
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Categories.ToListAsync());
+    }
+
+    // GET: Categories/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Categories/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Name,DisplayOrder")] Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+}
---
+++ PurvoZadanie/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using PurvoZadanie.Models;
+using System.Diagnostics;
+
+namespace PurvoZadanie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
